598415f23ec7b346d8d8b67f2f440d78cae4617d
Angold-4 <wangold4w@gmail.com>
2021-12-17 16:49:51 +0800

    test command line exec

--- tsma.php
+++ tsma.php
@@ -105,8 +105,11 @@ return false;
     $end = htmlspecialchars($_POST['end']);
     $offset = htmlspecialchars($_POST['offset']);
     $money = htmlspecialchars($_POST['money']);
+    $command = "/var/www/wfw/src/carbon/indicator/stock/a.out 'sma_single_earn' $company $start $end $offset $money";
 
-    $ret = shell_exec("/var/www/wfw/src/carbon/indicator/stock/a.out 'sma_single_earn' $company $start $end $offset $money");
+    echo "<pre>$command</pre>"
+
+    $ret = shell_exec($command);
 
     echo "<pre>$ret</pre>"
   ?>
